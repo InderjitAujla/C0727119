5c3ac44e80f6f242b04158c63c96107a6b42c35a
visualstudio <visualstudio@230-PC36>
2019-01-31 15:07:32 -0500

    commit code 3:07

--- C0727119/Program.cs
+++ C0727119/Program.cs
@@ -4,12 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C0727119
+namespace week_4
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+
         }
-    }
-}
+        class Village
+        {
+            public static int numberOfVillages = 0;
+            public Village nextVillage;
+            public Village previousVillage;
+            public string VillageName;
+            public bool isAstrildeHere = false;
+
+            public Village()
+            {
+                Village.numberOfVillages++;
+            }
+        }
+        class Countryside
+        {
+            public Village Maple;
+            public Village Toronto;
+            public Village Ajax;
+
+            public void MapInitializer()
+            {
+                Maple = new Village();
+                Maple.VillageName = "Maple";
+                Maple.previousVillage = null;
+                Maple.nextVillage = Toronto;
+                Toronto = new Village();
+                Toronto.previousVillage = Maple;
+
+
+                Toronto.VillageName = "Toronto";
+                Toronto.nextVillage = Ajax;
+                Ajax = new Village();
+                Ajax.VillageName = "Ajax";
+                Ajax.VillageName = null;
+                Ajax.previousVillage = Toronto;
+            }
+        }
+        public void LookForAstrilde()
+        {
+            //gggg
+        }
